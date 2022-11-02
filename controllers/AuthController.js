be742ea2ccacd2be646478ff31227369fa87a5bc
Julius Caezar <julius.caezar25@gmail.com>
2022-11-02 18:11:27 -0400

    loginPost function containing find user verification and check if password match

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,6 +7,36 @@ module.exports = class AuthController {
     res.render('auth/login')
   }
 
+  static async loginPost(req, res) {
+    const { email, password } = req.body
+
+    //find user
+    const user = await User.findOne({ where: { email: email } })
+
+    if (!user) {
+      req.flash('message', 'Usuário não encontrado')
+      res.render('auth/login')
+      return
+    }
+
+    //check if password match
+  const passwordMatch = bcrypt.compareSync(password, user.password)
+
+  if(!passwordMatch) {
+    req.flash('message', 'Senha inválida')
+    res.render('auth/login')
+    return
+  }
+
+  req.session.userid = user.id
+  req.flash('message', 'Autenticação realizada com sucesso!')
+
+  req.session.save(() => {
+    res.redirect('/')
+  })
+
+  }
+
   static register(req, res) {
     res.render('auth/register')
   }
@@ -31,7 +61,7 @@ module.exports = class AuthController {
     }
 
     //create a password
-    const salt = bcrypt.genSaltSync(10)
+    const salt =  bcrypt.genSaltSync(10)
     const hashedPassword = bcrypt.hashSync(password, salt)
 
     const user = {
@@ -41,7 +71,7 @@ module.exports = class AuthController {
     }
 
     try {
-    const createdUser =   await User.create(user)
+      const createdUser = await User.create(user)
 
       //initialize session
       req.session.userid = createdUser.id
