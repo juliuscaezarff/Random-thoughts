27406eb6b7f37e24265e3286c2f9c897bfc6e53d
Julius Caezar <julius.caezar25@gmail.com>
2022-11-14 11:14:37 -0400

    create removeTought and more correction

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -7,25 +7,30 @@ module.exports = class ToughtsController {
   }
 
   static async dashboard(req, res) {
-
     const userId = req.session.userid
 
     const user = await User.findOne({
       where: {
-        id: userId,
+        id: userId
       },
       include: Tought,
-      plain: true,
+      plain: true
     })
 
     //check if user exists
-    if(!user) {
+    if (!user) {
       res.redirect('/login')
     }
 
-    const toughts = user.Toughts.map((result) => result.dataValues)
+    const toughts = user.Toughts.map(result => result.dataValues)
 
-    res.render('toughts/dashboard', {toughts})
+    let emptyToughts = false
+
+    if(toughts.length === 0) {
+      emptyToughts = true
+    }
+
+    res.render('toughts/dashboard', { toughts, emptyToughts })
   }
 
   static createTought(req, res) {
@@ -42,6 +47,23 @@ module.exports = class ToughtsController {
       await Tought.create(tought)
 
       req.flash('message', 'Pensamento criado com sucesso!')
+
+      req.session.save(() => {
+        res.redirect('/toughts/dashboard')
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  static async removeTought(req, res) {
+    const id = req.body.id
+    const UserId = req.session.userid
+
+    try {
+      await Tought.destroy({ where: { id: id, UserId: UserId } })
+
+      req.flash('message', 'Pensamento removido com sucesso!')
 
       req.session.save(() => {
         res.redirect('/toughts/dashboard')
