60e00ca7efdd5ede2b5647e0e570c97427158025
Julius Caezar <julius.caezar25@gmail.com>
2022-11-14 09:41:17 -0400

    create the function createTought

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -9,4 +9,8 @@ module.exports = class ToughtsController {
   static async dashboard(req, res) {
     res.render('toughts/dashboard')
   }
+
+  static createTought(req, res) {
+    res.render('toughts/create')
+  }
 }
